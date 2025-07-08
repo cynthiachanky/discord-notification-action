0a860bed3b65cb2735a748d98272a2395674e6c8
Cynthia Chan <cynthiachanky@gmail.com>
2025-07-09 00:49:49 +0800

    add utility script extract-release.sh

--- entrypoint/release.sh
+++ entrypoint/release.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
-RELEASE=$(
-  curl -s -L --oauth2-bearer ${GITHUB_TOKEN} \
-  -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" \
-  "https://api.github.com/repos/${GITHUB_REPO}/releases/tags/${GITHUB_TAG}"
-)
-
-echo "> Extracting release information from GitHub..."
-RELEASE_ID=$(echo -E ${RELEASE} | jq -r ".id")
-RELEASE_NAME=$(echo -E ${RELEASE} | jq -r ".name")
-RELEASE_NOTE=$(echo -E ${RELEASE} | jq -r ".body")
+./entrypoint/utilities/extract-release.sh
 
 echo "> Posting release note to Discord..."
-MESSAGE="# :new: [${RELEASE_NAME}](https://github.com/${GITHUB_REPO}/releases/tag/${GITHUB_TAG}) is available now."$'\r\n\r\n'"${RELEASE_NOTE}"
+MESSAGE="$(cat release_message)"
 if [[ ${DISCORD_WEBHOOK_NAME} == "" && ${DISCORD_WEBHOOK_AVATAR} == "" ]]
 then
   echo "Using default name and avatar"
@@ -43,7 +34,6 @@ echo "Status: ${STATUS_CODE}"
 if [ ${STATUS_CODE} -ne 204 ]; then exit 1; fi
 
 echo "> Exporting release data to Action..."
-echo "release_id=${RELEASE_ID}" >> "${GITHUB_OUTPUT}"
-echo "release_name=${RELEASE_NAME}" >> "${GITHUB_OUTPUT}"
+echo "$(cat release_meta)" >> "${GITHUB_OUTPUT}"
 
 exit 0
